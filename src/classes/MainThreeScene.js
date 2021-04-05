1b8abb44b1979012ebc4b0021520302054f3b290
unknown <ballot.marius@gmail.com>
2021-04-05 11:39:01 +0200

    first commit

---
+++ src/classes/MainThreeScene.js
@@ -0,0 +1,73 @@
+import * as THREE from "three"
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+import RAF from '../utils/RAF'
+import config from '../utils/config'
+import MyGUI from '../utils/MyGUI'
+
+import simpleFrag from '../shaders/simple.frag'
+import simpleVert from '../shaders/simple.vert'
+
+class MainThreeScene {
+    constructor() {
+        this.bind()
+        this.camera
+        this.scene
+        this.renderer
+        this.controls
+    }
+
+    init(container) {
+        //RENDERER SETUP
+        this.renderer = new THREE.WebGLRenderer({ antialias: true })
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+        this.renderer.debug.checkShaderErrors = true
+        container.appendChild(this.renderer.domElement)
+
+        //MAIN SCENE INSTANCE
+        this.scene = new THREE.Scene()
+
+        //CAMERA AND ORBIT CONTROLLER
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+        this.camera.position.set(0, 0, 5)
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+        this.controls.enabled = config.controls
+        this.controls.maxDistance = 1500
+        this.controls.minDistance = 0
+
+        //DUMMY CUBE + SIMPLE GLSL SHADER LINKAGE
+        const shaderMat = new THREE.ShaderMaterial({
+            vertexShader: simpleVert,
+            fragmentShader: simpleFrag,
+        })
+        const cube = new THREE.Mesh(new THREE.BoxGeometry(), shaderMat)
+        this.scene.add(cube)
+
+        if (config.myGui)
+            MyGUI.start()
+
+        //RENDER LOOP AND WINDOW SIZE UPDATER SETUP
+        window.addEventListener("resize", this.resizeCanvas)
+        RAF.subscribe('threeSceneUpdate', this.update)
+    }
+
+    update() {
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    resizeCanvas() {
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+        this.camera.aspect = window.innerWidth / window.innerHeight
+        this.camera.updateProjectionMatrix()
+    }
+
+    bind() {
+        this.resizeCanvas = this.resizeCanvas.bind(this)
+        this.update = this.update.bind(this)
+        this.init = this.init.bind(this)
+    }
+}
+
+const _instance = new MainThreeScene()
+export default _instance
---
+++ src/utils/MyGUI.js
@@ -0,0 +1,21 @@
+import { GUI } from "three/examples/jsm/libs/dat.gui.module.js";
+
+
+class MyGui {
+    constructor() {
+        this.bind()
+    }
+
+    start() {
+        this.gui = new GUI();
+
+    }
+
+    bind() {
+        this.start = this.start.bind(this)
+    }
+}
+
+
+const _instance = new MyGui
+export default _instance
